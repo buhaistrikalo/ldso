bb004203e9815daa942bd7c2f15897280030eef6
Kirill <kirilltcoi@yandex.ru>
2021-10-05 14:20:50 +0700

    quiz class > func + add hooks

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,83 +1,58 @@
-import {React, Component} from 'react';
+import React, {useState} from 'react';
 import "./quiz.css";
 import Counter from './counter/counter';
 import Question from './question/question';
 import Answers from './answers/answers';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    location,
-    match,
-    matchPath,
     Redirect,
 } from 'react-router-dom';
 
 import Questions from '../json/q.json'; // question bank 
-import Score from './score';
 
-class Quiz extends Component { //сколько вариантов ответа
-    state = {
-        questions: Questions,
-        current: 0,
-        score: 0,
-        showScore: false,
-    }
+const Quiz = () => { //сколько вариантов ответа
+    const [questions, setQuestions] = useState(Questions)
+    const [current, setCurrent] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
 
-    updateData = (theTrue) => {
+    const updateData = (theTrue) => {
         if (theTrue)
         {
-            this.setState({score: this.state.score + 1})
+            setScore(score + 1)
         }
-        if (this.state.current + 1 < this.state.questions.length)
+        if (current + 1 < questions.length)
         {
-            this.setState({current: this.state.current + 1})
+            setCurrent(current + 1)
         } else
         {
-            this.setState({showScore: true})
+            setShowScore(true)
         }
-    }
+    };
 
-    resetQuiz() {
-        this.setState({current: 0})
-        this.setState({score: 0})
-        this.setState({showScore: false})
-    }
-
-
-    render() {
-        return (
-            // <Score score={this.state.score} count={this.state.questions.length} />
-            <div className="quiz">
-                {this.state.showScore
-                    ? <Redirect to={{
-                        pathname: '/score',
-                        state: {
-                            score: this.state.score,
-                            count: this.state.questions.length,
-                        }
-                    }} />
-                    : <div className="quiz__pull">
-                        <div className="quiz__text">
-                            <Counter count={this.state.current + 1} length={this.state.questions.length} />
-                            <div className="wrapper">
-                                <Question question={this.state.questions[this.state.current].question} />
-                                <div className="quiz__desc">Выберете один правильный вариант ответа</div>
-                            </div>
+    return (
+        // <Score score={score} count={questions.length} />
+        <div className="quiz">
+            {showScore
+                ? <Redirect to={{
+                    pathname: '/score',
+                    state: {
+                        score: score,
+                        count: questions.length,
+                    }
+                }} />
+                : <div className="quiz__pull">
+                    <div className="quiz__text">
+                        <Counter count={current + 1} length={questions.length} />
+                        <div className="wrapper">
+                            <Question question={questions[current].question} />
+                            <div className="quiz__desc">Выберете один правильный вариант ответа</div>
                         </div>
-                        <Answers updateData={this.updateData} answers={this.state.questions[this.state.current].answers} />
                     </div>
-                }
-            </div>
-        );
-    }
+                    <Answers updateData={updateData} answers={questions[current].answers} />
+                </div>
+            }
+        </div>
+    );
 }
-
-// function getFile(path) {
-//     let file = JSON.parse(fs.readFileSync('./json/q.json'))
-
-// }
-
 
 export default Quiz;
--- src/components/score.jsx
+++ src/components/score.jsx
@@ -6,7 +6,7 @@ const Score = (props) => {
         <div>
             <div className="quiz__score">
                 <div>Правильных ответов {props.location.state.score} из {props.location.state.count}</div>
-                <Link to='/quiz'><button className='button button_black button_mt50'
+                <Link to='/'><button className='button button_black button_mt50'
                 >Пройти заново</button></Link>
             </div>
         </div>
